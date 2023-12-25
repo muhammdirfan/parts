fc1e11861c04b6aa2f7e5e7a40b466c3ae6a74c2
muhammdirfan <mirfan062231@gmail.com>
2023-12-26 02:58:28 +0500

    first commit

---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,30 @@
+// react
+import React from "react";
+// third-party
+import { FormattedMessage } from "react-intl";
+// application
+import AppLink from "~/components/shared/AppLink";
+import url from "~/services/url";
+
+import { LogoCOLORHSvg } from "~/svg";
+
+interface Props extends React.HTMLAttributes<HTMLElement> {}
+
+function Logo(props: Props) {
+    return (
+        <div {...props}>
+            <AppLink href={url.home()} className="logo">
+                <div className="logo__slogan">
+                    <FormattedMessage id="TEXT_SLOGAN" />
+                </div>
+                <div className="logo__image">
+                    {/* logo */}
+                    <LogoCOLORHSvg />
+                    {/* logo / end */}
+                </div>
+            </AppLink>
+        </div>
+    );
+}
+
+export default Logo;
